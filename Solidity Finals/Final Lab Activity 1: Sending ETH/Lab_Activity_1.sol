23dcbf8e713b74ce9dedd8f44c0785017fe25079
johnPhilippe <99706118+johnPhilippe@users.noreply.github.com>
2024-03-19 14:55:47 +0800

    Add files via upload

---
+++ Solidity Finals/Final Lab Activity 1: Sending ETH/Lab_Activity_1.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+contract EtherWallet {
+    address payable public owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    receive() external payable {}
+
+    function withdraw(uint256 _amount) public {
+        require(msg.sender == owner, "Only the owner can withdraw");
+        require(_amount <= address(this).balance, "Insufficient balance");
+        owner.transfer(_amount);
+    }
+
+    function getBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+}
